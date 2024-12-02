41b9e16df335898a3facade4264f1bdcf9b3db5a
shirel-maymon <shirelmaymon@hilma.tech>
2024-12-02 15:42:01 +0200

    modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentModule } from './student/student.module';
+import { ChapterModule } from './chapter/chapter.module';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
-  imports: [],
+  imports: [StudentModule, ChapterModule, LessonModule],
   controllers: [AppController],
   providers: [AppService],
 })
